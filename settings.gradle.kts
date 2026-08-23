8db398fb1b44f86bb8c62cf9505a76efaa608696
BearJ3rk <shadelessnight@gmail.com>
2026-08-23 12:06:44 -0700

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BearJ3rksNerdScanner"
+include(":app")
