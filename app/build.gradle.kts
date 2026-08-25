74ccbb8b9b03af21092bfafb3fecf3f079a2ae65
BearJ3rk <shadelessnight@gmail.com>
2026-08-25 15:44:07 -0700

    Add V0.2.1 updater: app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bearj3rk.nerdscanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.2.1"
     }
 
     buildTypes {
