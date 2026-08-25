90727c287ad0d30bb95b8fb8601c2e51958bc867
BearJ3rk <shadelessnight@gmail.com>
2026-08-25 14:45:36 -0700

    Add SVG set symbol support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     implementation("com.google.mlkit:text-recognition:16.0.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("io.coil-kt:coil:2.6.0")
+    implementation("io.coil-kt:coil-svg:2.6.0")
 }
