904ce4c4693e0da2ec645ee9dba67a131ca842a4
BearJ3rk <shadelessnight@gmail.com>
2026-08-26 11:08:01 -0700

    Release V0.7: cached artwork printing matching

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bearj3rk.nerdscanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.6"
+        versionCode = 8
+        versionName = "0.7"
     }
 
     signingConfigs {
