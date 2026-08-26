c23245477456f5f174cca603fb48e4e33846da52
BearJ3rk <shadelessnight@gmail.com>
2026-08-26 16:06:55 -0700

    Release V0.8: reliable cached artwork loading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bearj3rk.nerdscanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.7"
+        versionCode = 9
+        versionName = "0.8"
     }
 
     signingConfigs {
