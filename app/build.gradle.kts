ccd45ad339ee1f645fef0157bb5bb7207f96d5e4
BearJ3rk <shadelessnight@gmail.com>
2026-09-09 12:23:48 -0700

    Release V0.16: downloadable visual recognition database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.bearj3rk.nerdscanner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.15"
+        versionCode = 17
+        versionName = "0.16"
     }
 
     signingConfigs {
